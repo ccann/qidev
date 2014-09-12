7afe2cea649e2dc077b57a8e4505743b4f64dd38
ccann <cocanning11@gmail.com>
2014-09-12 18:09:06 -0400

    support volume, life, etc.

--- lib/clio.py
+++ lib/clio.py
@@ -3,147 +3,173 @@ from clint.textui import puts, indent
 from tabulate import tabulate
 import readline
 
+completions = list()
 
-class IO():
 
-    def __init__(self):
-        self.completions = list()
+def bold(text):
+    return '\033[1m' + text + '\033[0m'
 
-    def bold(self, text):
-        return '\033[1m' + text + '\033[0m'
 
-    def underlined(self, text):
-        return '\033[4m' + text + '\033[0m'
+def underlined(text):
+    return '\033[4m' + text + '\033[0m'
 
-    def completer(self, text, state):
-        options = [i for i in self.completions if i.startswith(text)]
-        if state < len(options):
-            return options[state]
-        else:
-            return None
 
-    def show_installed_packages(self, pkg_data):
-        """Pretty-print a table of installed packages."""
-        table = list()
-        for d in pkg_data:
-            n_behaviors = len(d['behaviors'])
-            n_behaviors = str(n_behaviors) if not n_behaviors == 0 else None
-            n_services = len(d['services'])
-            n_services = str(n_services) if not n_services == 0 else None
-            table.append([self.bold(d['langToName']['en_US']),
-                          d['version'],
-                          n_behaviors,
-                          n_services])
-        print ''
-        print tabulate(table,
-                       headers=['Package Name', 'Version', 'Behaviors', 'Services'],
-                       tablefmt="orgtbl")
-        print ''
+def completer(text, state):
+    options = [i for i in completions if i.startswith(text)]
+    if state < len(options):
+        return options[state]
+    else:
+        return None
 
-    def show_installed_services(self, pkg_data):
-        """Pretty-print a table of installed services."""
-        table = list()
-        for d in pkg_data:
-            for s in d['services']:
-                table.append([self.bold(d['langToName']['en_US']),
-                              (s['name']),
-                              col.green('True') if s['autoRun'] else col.red('False')])
-        print ''
-        print tabulate(table,
-                       headers=['Package Name', 'Service Name', 'Autorun?'],
-                       tablefmt='orgtbl')
-        print ''
 
-    def prompt_for_package(self, pkg_data):
-        """Prompt the user to specify the package name."""
-        pkg_uuids = [p['uuid'] for p in pkg_data]
-        pkg_names = [p['langToName']['en_US'] for p in pkg_data]
-        self.completions = pkg_uuids + pkg_names
-        readline.set_completer_delims('')
-        readline.parse_and_bind("tab: complete")
-        readline.set_completer(self.completer)
-        inp = raw_input('Enter package name or UUID (tab to complete)\n> ')
-        self.show_package_details(inp, pkg_data)
+def show_installed_packages(pkg_data):
+    """Pretty-print a table of installed packages."""
+    table = list()
+    for d in pkg_data:
+        n_behaviors = len(d['behaviors'])
+        n_behaviors = str(n_behaviors) if not n_behaviors == 0 else None
+        n_services = len(d['services'])
+        n_services = str(n_services) if not n_services == 0 else None
+        table.append([bold(d['langToName']['en_US']),
+                      d['version'],
+                      n_behaviors,
+                      n_services])
+    print ''
+    print tabulate(table,
+                   headers=['Package Name', 'Version', 'Behaviors', 'Services'],
+                   tablefmt="orgtbl")
+    print ''
 
-    def prompt_for_behavior(self, behaviors):
-        """Prompt the user to specify behavior name."""
-        self.completions = behaviors
-        readline.set_completer_delims('')
-        readline.parse_and_bind("tab: complete")
-        readline.set_completer(self.completer)
-        inp = raw_input('Enter package-uuid/behavior-name (tab to complete)\n> ')
-        return inp
 
-    def pretty(self, o):
-        """Format objects nicely."""
-        if isinstance(o, list):
-            return ', '.join(map(str, o))
-        elif isinstance(o, str):
-            return '{}: '.format(col.blue(o))
-        else:
-            return None
+def show_installed_services(pkg_data):
+    """Pretty-print a table of installed services."""
+    table = list()
+    for d in pkg_data:
+        for s in d['services']:
+            table.append([bold(d['langToName']['en_US']),
+                          (s['name']),
+                          col.green('True') if s['autoRun'] else col.red('False')])
+    print ''
+    print tabulate(table,
+                   headers=['Package Name', 'Service Name', 'Autorun?'],
+                   tablefmt='orgtbl')
+    print ''
 
-    def put_behavior_string(self, b):
-        """Print information about a behavior."""
-        try:
-            name = b['langToName']['en_US']
-            puts(self.bold('* {} (behavior)'.format(self.bold(name))))
+
+def prompt_for_package(pkg_data):
+    """Prompt the user to specify the package name."""
+    global completions
+    pkg_uuids = [p['uuid'] for p in pkg_data]
+    pkg_names = [p['langToName']['en_US'] for p in pkg_data]
+    completions = pkg_uuids + pkg_names
+    readline.set_completer_delims('')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
+    inp = raw_input('Enter package name or UUID (tab to complete)\n> ')
+    show_package_details(inp, pkg_data)
+
+
+def prompt_for_behavior(behaviors):
+    """Prompt the user to specify behavior name."""
+    global completions
+    completions = behaviors
+    readline.set_completer_delims('')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
+    inp = raw_input('Enter package-uuid/behavior-name (tab to complete)\n> ')
+    return inp
+
+
+def pretty(o):
+    """Format objects nicely."""
+    if isinstance(o, list):
+        return ', '.join(map(str, o))
+    elif isinstance(o, str):
+        return '{}: '.format(col.blue(o))
+    else:
+        return None
+
+
+def put_behavior_string(b):
+    """Print information about a behavior."""
+    try:
+        name = b['langToName']['en_US']
+        puts(bold('* {} (behavior)'.format(bold(name))))
+        with indent(4):
+            nat = b['nature']
+            nature = col.red(nat) if nat == 'interactive' else col.blue(nat)
+            puts(pretty('Nature') + nature)
+    except KeyError:
+        pass
+
+
+def put_service_string(s):
+    """Print information about a service."""
+    try:
+        name = s['name']
+        puts(bold('* {} (service)'.format(bold(name))))
+        with indent(4):
+            puts(pretty('AutoRun') + 'True' if s['autoRun'] else 'False')
+    except KeyError:
+        pass
+
+
+def show_package_details(package, pkg_data):
+    """Pretty-print the details of an installed package."""
+    print('')
+    for p in pkg_data:
+        if p['langToName']['en_US'] == package or p['uuid'] == package:
+            print('* {} ({})'.format(bold(p['langToName']['en_US']),
+                                     col.cyan('v' + p['version'])))
             with indent(4):
-                nat = b['nature']
-                nature = col.red(nat) if nat == 'interactive' else col.blue(nat)
-                puts(self.pretty('Nature') + nature)
-        except KeyError:
-            pass
+                puts(pretty('UUID') + p['uuid'])
+                try:
+                    nr = p['naoqiRequirements'][0]
+                    mn = nr['minVersion']
+                    mx = ' to {}'.format(nr['maxVersion']) if nr['maxVersion'] else ' +'
+                    puts(pretty('NaoQi Requirements') + mn + mx)
+                except IndexError:
+                    pass
+                puts(pretty('Supported Languages') +
+                     pretty(p['supportedLanguages']))
+                try:
+                    puts(pretty('Desciption') + p['langToDesc']['en_US'])
+                except KeyError:
+                    pass
+                for b in p['behaviors']:
+                    put_behavior_string(b)
+                for s in p['services']:
+                    put_service_string(s)
+    print('')
 
-    def put_service_string(self, s):
-        """Print information about a service."""
-        try:
-            name = s['name']
-            puts(self.bold('* {} (service)'.format(self.bold(name))))
+
+def show_running(behaviors, services, pkg_data):
+    """Print running behaviors and services."""
+    print('\nActive Content:')
+    if not behaviors and not services:
+        print('No behaviors or services are active.')
+    if behaviors:
+        for b in behaviors:
             with indent(4):
-                puts(self.pretty('AutoRun') + 'True' if s['autoRun'] else 'False')
-        except KeyError:
-            pass
+                puts('* {}  ({})'.format(bold(b), col.magenta('behavior')))
+    if services:
+        for s in services:
+            with indent(4):
+                puts('* {}  ({})'.format(bold(s), col.blue('service')))
+    print('')
 
-    def show_package_details(self, package, pkg_data):
-        """Pretty-print the details of an installed package."""
-        print('')
-        for p in pkg_data:
-            if p['langToName']['en_US'] == package or p['uuid'] == package:
-                print('* {} ({})'.format(self.bold(p['langToName']['en_US']),
-                                         col.cyan('v' + p['version'])))
-                with indent(4):
-                    puts(self.pretty('UUID') + p['uuid'])
-                    try:
-                        nr = p['naoqiRequirements'][0]
-                        mn = nr['minVersion']
-                        mx = ' to {}'.format(nr['maxVersion']) if nr['maxVersion'] else ' +'
-                        puts(self.pretty('NaoQi Requirements') + mn + mx)
-                    except IndexError:
-                        pass
-                    puts(self.pretty('Supported Languages') +
-                         self.pretty(p['supportedLanguages']))
-                    try:
-                        puts(self.pretty('Desciption') + p['langToDesc']['en_US'])
-                    except KeyError:
-                        pass
-                    for b in p['behaviors']:
-                        self.put_behavior_string(b)
-                    for s in p['services']:
-                        self.put_service_string(s)
-        print('')
 
-    def show_running(self, behaviors, services, pkg_data):
-        """Print running behaviors and services."""
-        print('\nActive Content:')
-        if not behaviors and not services:
-            print('No behaviors or services are active.')
-        if behaviors:
-            for b in behaviors:
-                with indent(4):
-                    puts('* {}  ({})'.format(self.bold(b), col.magenta('behavior')))
-        if services:
-            for s in services:
-                with indent(4):
-                    puts('* {}  ({})'.format(self.bold(s), col.blue('service')))
-        print('')
+def format_nao_output(file_like, command):
+    done = False
+    for line in file_like:
+        if (command == 'restart' or command == 'start') and 'Starting naoqi' in line:
+            done = True
+        elif command == 'stop' and 'Naoqi stopped' in line:
+            done = True
+        line = line.replace('Stopping', str(col.red('Stopping')))
+        line = line.replace('Starting', str(col.green('Starting')))
+        line = line.replace('waiting', str(col.yellow('waiting')))
+        print line.rstrip()
+        if done:
+            break
+    file_like.close()
---
+++ lib/handlers.py
@@ -0,0 +1,171 @@
+import os
+import socket
+import qi
+import clio as io
+import config
+from connection import Connection
+from clint.textui import colored as col
+
+
+def verbose_print(flag):
+    """Print function for --verbose flag."""
+    def func(text):
+        if flag:
+            print(text)
+    return func
+
+
+def create_connection(ns, verb, ssh=True, create_session=True):
+    """Establish a connection to hostname and a qi session."""
+    hostname = config.read_hostname()
+    if create_session:
+        try:
+            session = qi.Session(hostname)
+        except RuntimeError:
+            raise RuntimeError('%s: could not establish connection to %s' %
+                               (col.red('ERROR'), col.blue(hostname)))
+    else:
+        session = None
+    try:
+        verb('connect to {}'.format(hostname))
+        conn = Connection(hostname, ssh=ssh)
+    except socket.gaierror as e:
+        raise RuntimeError('{}: {} ... for hostname: {}'.format(col.red('ERROR'), e,
+                                                                col.blue(hostname)))
+    except socket.error as e:
+        # assuming this is a virtual bot... kind of a hack
+        conn = Connection(hostname, virtual=True, ssh=ssh)
+    return conn, session
+
+
+def install_handler(ns):
+    """Install a package to a remote host or locally."""
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb)
+    try:
+        verb('Create package from directory: {}'.format(ns.p))
+        abs_path = conn.create_package(ns.p)
+        verb('Transfer package to {}'.format(conn.hostname))
+        pkg_name = conn.transfer(abs_path)
+        verb('Install package: {}'.format(pkg_name))
+        conn.install_package(session, abs_path)
+        verb('Clean up: {}'.format(pkg_name))
+        conn.delete_pkg_file(abs_path)
+    except IOError:
+        if ns.p:
+            print('%s: %s is not a project directory (does not contain manifest.xml)' %
+                  (col.red('ERROR'), col.blue(ns.p)))
+        else:
+            print('%s: %s is not a project directory (does not contain manifest.xml)' %
+                  (col.red('ERROR'), col.blue(os.getcwd())))
+
+
+def config_handler(ns):
+    """Configure fields of the .qidev file."""
+    verb = verbose_print(ns.verbose)
+    field = ns.field.strip()
+    if field == 'hostname':
+        verb('Set {} to {}'.format(field, ns.value))
+        config.write_hostname(ns.value)
+    else:
+        print('ERROR: unsupported field {}'.format(field))
+
+
+def connect_handler(ns):
+    """Change hostname field of the .qidev file."""
+    verb = verbose_print(ns.verbose)
+    verb('Set hostname to {}'.format(ns.hostname))
+    config.write_hostname(ns.hostname)
+
+
+def show_handler(ns):
+    """Show information about a package, service, active content, etc."""
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, ssh=False)
+    pkg_data = conn.get_installed_package_data(session)
+    if ns.s:
+        io.show_installed_services(pkg_data)
+    elif ns.i:
+        io.prompt_for_package(pkg_data)
+    elif ns.active:
+        io.show_running(conn.get_running_behaviors(session),
+                        conn.get_running_services(session),
+                        pkg_data)
+    else:
+        io.show_installed_packages(pkg_data)
+
+
+def state_handler(state, ns):
+    """Start or stop a behavior or service."""
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, ssh=False)
+    if state == 'start':
+        services = conn.get_declared_services(session)
+        behaviors = conn.get_installed_behaviors(session)
+        inp = io.prompt_for_behavior(services + behaviors)
+        if inp in services:
+            verb('start service: {}'.format(inp))
+            conn.start_service(session, inp)
+        elif inp in behaviors:
+            if ns.bm:
+                verb('start behavior: {}'.format(inp))
+                conn.start_behavior(session, inp)
+            else:
+                try:
+                    verb('switch focus to: {}'.format(inp))
+                    conn.life_switch_focus(session, inp)
+                except RuntimeError:  # this is a huge hack but its not my fault
+                    verb('couldnt find behavior {} so appending "/.": {}'.format(inp+'/.'))
+        else:
+            print('{}: {} is not an eligible behavior or service'.format(col.red('ERROR'),
+                                                                         inp))
+    elif state == 'stop':
+        if not ns.bm:  # stop focused activity
+            conn.life_stop_focus(session)
+        else:  # stop behavior manually with bm
+            services = conn.get_running_services(session)
+            behaviors = conn.get_running_behaviors(session)
+            inp = io.prompt_for_behavior(services + behaviors)
+            if inp in services:
+                conn.stop_service(session, inp)
+            elif inp in behaviors:
+                conn.stop_behavior(session, inp)
+            else:
+                print('{}: {} is not an eligible behavior or service'.format(col.red('ERROR'),
+                                                                             inp))
+
+
+def life_handler(ns):
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, ssh=False)
+    if ns.state == 'on':
+        conn.life_on(session)
+    elif ns.state == 'off':
+        conn.life_off(session)
+    else:
+        print('life state can only be on or off')
+
+
+def nao_handler(ns):
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, create_session=False)
+    verb('nao action: {}'.format(ns.action))
+    sshin, sshout, ssherr = conn.ssh.exec_command('sudo /etc/init.d/naoqi %s' % ns.action)
+    io.format_nao_output(sshout, ns.action)
+
+
+def power_handler(ns):
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, ssh=False)
+    verb('power action: {}'.format(ns.action))
+    if ns.action == 'reboot':
+        conn.robot_reboot(session)
+    if ns.action == 'shutdown':
+        conn.robot_shutdown(session)
+
+
+def volume_handler(ns):
+    verb = verbose_print(ns.verbose)
+    conn, session = create_connection(ns, verb, ssh=False)
+    verb('volume level: {}'.format(ns.level))
+    conn.set_volume(session, ns.level)
